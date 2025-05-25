d3c777adfb5d9f5c7ba3c648e03c7882d1248c8d
Phaethon <do_rayaki_@outlook.co.th>
2025-05-25 15:13:58 +0700

    feat(login): implement server action

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,18 +1,41 @@
+"use client";
+
+import { FormState, loginAction } from "@/actions/login-action";
+import { useActionState } from "react";
+
+const initialState: FormState = {
+  success: undefined,
+  message: "",
+  email: "",
+};
+
 export default function LoginPage() {
+  const [formState, formAction] = useActionState(loginAction, initialState);
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="bg-white shadow-lg rounded-lg p-8 w-full max-w-md">
         <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
-        {/* Alert placeholder */}
-        <div className="mb-4 space-y-2">
-          <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative">
-            <span className="block sm:inline">Invalid email or password.</span>
+
+        {/* Start Alert placeholder */}
+
+        {formState.success !== undefined && (
+          <div className="mb-4 space-y-2">
+            <div
+              className={`${
+                formState.success
+                  ? "bg-green-100 border border-green-400 text-green-700"
+                  : "bg-red-100 border border-red-400 text-red-700"
+              } px-4 py-3 rounded relative`}
+            >
+              <span className="block sm:inline">{formState.message}</span>
+            </div>
           </div>
-          <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative">
-            <span className="block sm:inline">Login successful!</span>
-          </div>
-        </div>
-        <form className="space-y-5">
+        )}
+
+        {/* End Alert placeholder */}
+
+        <form className="space-y-5" action={formAction}>
           <div>
             <label
               htmlFor="email"
@@ -27,6 +50,7 @@ export default function LoginPage() {
               className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
               required
               autoComplete="email"
+              defaultValue={formState.email}
             />
           </div>
           <div>
