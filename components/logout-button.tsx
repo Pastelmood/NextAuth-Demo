c3cee6eac6bdca08c7458191ae33900860911268
Phaethon <do_rayaki_@outlook.co.th>
2025-05-26 23:21:38 +0700

    chor(app): update

--- components/logout-button.tsx
+++ components/logout-button.tsx
@@ -2,10 +2,11 @@ import { logoutAction } from "@/actions/logout-action";
 
 export default function LogoutButton() {
   return (
-    <form action={logoutAction}>
-      <button className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-md transition-colors">
-        Logout
-      </button>
-    </form>
+    <button
+      className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-md transition-colors"
+      formAction={logoutAction}
+    >
+      Logout
+    </button>
   );
 }
